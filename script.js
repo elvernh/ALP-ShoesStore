0e4db0b535821d5df9744547874fef362c4eb028
elvernh <87465378+elvernh@users.noreply.github.com>
2024-05-28 13:20:26 +0700

    nibbers

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.getElementById("searchButton").addEventListener('click', function(){
-    window.location.href = 'https://www.google.com';
-})
+document.getElementById("searchButton").addEventListener("click", function () {
+  window.location.href = "https://www.google.com";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector("#hamburger");
+    const navMenu = document.querySelector("#nav-menu");
+  
+    hamburger.addEventListener("click", function () {
+      hamburger.classList.toggle("hamburger-active");
+      navMenu.classList.toggle("displayHidden");
+    });
+  });
