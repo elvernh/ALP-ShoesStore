a6044e4089ae4a53b7b000e8f7973127c677b133
elvernh <87465378+elvernh@users.noreply.github.com>
2024-05-30 13:38:49 +0700

    committed

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-document.getElementById("searchButton").addEventListener("click", function () {
-  window.location.href = "https://www.google.com";
-});
-
 const hamburger = document.querySelector('#hamburger');
-      const navMenu = document.querySelector('#nav-menu');
-      hamburger.addEventListener('click', function(){
-        hamburger.classList.toggle('hamburger-active');
-        navMenu.classList.toggle('hidden');
-      });
+const navMenu = document.querySelector('#nav-menu');
+const heartText = document.querySelector(".heartText");
+const heart = document.querySelector(".heart");
+const shop = document.querySelector(".shop");
+const shopText = document.querySelector(".shopText");
+const search = document.querySelector(".search");
+const searchText = document.querySelector(".searchText");
+
+        hamburger.addEventListener('click', function(){
+          console.log("Hamburger menu clicked");
+          hamburger.classList.toggle('hamburger-active');
+          navMenu.classList.toggle('hidden');
+          heartText.classList.toggle('hidden');
+          heart.classList.toggle('hidden');
+          shopText.classList.toggle('hidden');
+          shop.classList.toggle('hidden');
+          search.classList.toggle('hidden');
+          searchText.classList.toggle('hidden');
+        });
