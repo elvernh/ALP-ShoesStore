a460d2ab5355514ae39a7c353390a6b8802a33d3
elvernh <87465378+elvernh@users.noreply.github.com>
2024-05-30 10:53:47 +0700

    niggser

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ document.getElementById("searchButton").addEventListener("click", function () {
   window.location.href = "https://www.google.com";
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector("#hamburger");
-    const navMenu = document.querySelector("#nav-menu");
-  
-    hamburger.addEventListener("click", function () {
-      hamburger.classList.toggle("hamburger-active");
-      navMenu.classList.toggle("displayHidden");
-    });
-  });
+const hamburger = document.querySelector('#hamburger');
+      const navMenu = document.querySelector('#nav-menu');
+      hamburger.addEventListener('click', function(){
+        hamburger.classList.toggle('hamburger-active');
+        navMenu.classList.toggle('hidden');
+      });
